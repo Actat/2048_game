c4027ffc5dc8ce6c1661d0f8a809fc069dc2e65e
Actat <akio.toyosh@gmail.com>
2022-11-23 14:36:14 +0900

    複数回の試行の結果を表示

--- evaluate_player.cpp
+++ evaluate_player.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include "game_board.hpp"
 #include "game_master.hpp"
@@ -21,6 +22,40 @@ int main(int argc, char *argv[]) {
 
     result.at(i) = board;
   }
+
+  double average_score   = 0;
+  double average_largest = 0;
+  std::array<int, NUM_TRIAL> score;
+  std::array<int, NUM_TRIAL> largest_tile;
+  for (int i = 0; i < NUM_TRIAL; i++) {
+    score.at(i)        = result.at(i).get_score();
+    largest_tile.at(i) = result.at(i).get_largest_tile();
+    average_score += 1.0 * result.at(i).get_score() / NUM_TRIAL;
+    average_largest += 1.0 * result.at(i).get_largest_tile() / NUM_TRIAL;
+  }
+
+  std::cout << "Summary of " << NUM_TRIAL << " trials." << std::endl;
+  std::cout << "largest tile" << std::endl
+            << "\tbest:\t"
+            << *std::max_element(largest_tile.begin(), largest_tile.end())
+            << "\t("
+            << std::pow(2, *std::max_element(largest_tile.begin(),
+                                             largest_tile.end()))
+            << ")" << std::endl
+            << "\tworst:\t"
+            << *std::min_element(largest_tile.begin(), largest_tile.end())
+            << "\t("
+            << std::pow(2, *std::min_element(largest_tile.begin(),
+                                             largest_tile.end()))
+            << ")" << std::endl
+            << "\tave.:\t" << average_largest << "\t("
+            << std::pow(2, average_largest) << ")" << std::endl;
+  std::cout << "score" << std::endl
+            << "\tbest:\t" << *std::max_element(score.begin(), score.end())
+            << std::endl
+            << "\tworst:\t" << *std::min_element(score.begin(), score.end())
+            << std::endl
+            << "\tave.:\t" << average_score << std::endl;
 
   return 0;
 }
