93ecd86eb4cae1fe9e7ade8a1a145484f4e29ff9
Actat <akio.toyosh@gmail.com>
2022-12-21 18:15:43 +0900

    remove unorderd_map cache

--- game_board_eval.hpp
+++ game_board_eval.hpp
@@ -8,13 +8,6 @@
 
 class game_board_eval {
 private:
-  static std::unordered_map<std::array<int, BOARD_SIZE * BOARD_SIZE>,
-                            int,
-                            hash_board_array>
-      board_cache_;
-  static std::unordered_map<std::array<int, BOARD_SIZE>, int, hash_line_array>
-      line_cache_;
-
   int evaluate_line_(std::array<int, BOARD_SIZE> &line) const;
 
 public:
